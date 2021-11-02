53979bdb7e65ca3d5f1356508652597ed90f96d8
vahanmkrtumyan <89935857+vahanmkrtumyan90@users.noreply.github.com>
2021-11-02 16:16:40 +0400

    sliceing the components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-export default function Greeting() {
-  return <h4> this is Sasun and this is my first component</h4>;
+export default function Booklist() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg"
+    alt=""
+  />
+);
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => <h4>Amelia Hepworth and Tim Warnes</h4>;
+
+ReactDOM.render(<Booklist />, document.getElementById("root"));
