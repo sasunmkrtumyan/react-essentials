b325f0303380c126acac6beda3a05bba3b596a67
vahanmkrtumyan <89935857+vahanmkrtumyan90@users.noreply.github.com>
2021-11-03 19:00:23 +0400

    props in jsx

--- src/index.js
+++ src/index.js
@@ -6,32 +6,23 @@ export default function Booklist() {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
-
+const author = "Amelia Hepworth";
 const Book = () => {
+  const title = "I Love You to the Moon and Back";
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg"
+        alt="asd"
+      />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      <p></p>
     </article>
   );
 };
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg"
-    alt=""
-  />
-);
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => <h4>Amelia Hepworth and Tim Warnes</h4>;
 
 ReactDOM.render(<Booklist />, document.getElementById("root"));
