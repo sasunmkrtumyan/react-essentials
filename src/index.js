766073395c170c08f20f8b7dde53a40d53de4da4
vahanmkrtumyan <89935857+vahanmkrtumyan90@users.noreply.github.com>
2021-11-08 00:29:13 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 
 const books = [
   {
+    id: 1,
     author: "Amelia Hepworth",
     title: "I Love You to the Moon and Back",
     img: "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg",
   },
   {
+    id: 2,
     author: "Shannon Olsen",
     title: "Our Class is a Family",
     img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg",
@@ -20,20 +22,25 @@ const names = ["john", "peter", "susan"];
 function Booklist() {
   return (
     <section className="booklist">
-      {books.map((book) => {
-        return <Book book={book}></Book>;
+      {books.map((book, index) => {
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { author, title, img } = props.book;
+const Book = ({ author, title, img }) => {
+  const clickHandler = () => {
+    alert(title);
+  };
   return (
     <article className="book">
       <img src={img} alt="asd" />
-      <h1>{title}</h1>
+      <h1 onClick={() => alert(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        button
+      </button>
     </article>
   );
 };
