4dd4705e39d4126c7e7fbce104b1a82f1a84bd45
vahanmkrtumyan <sasunmkrtumyan92@gmail.com>
2021-11-27 14:20:01 +0400

    added: counter

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-// cleanup function
-// second parameter
+
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
   useEffect(() => {
